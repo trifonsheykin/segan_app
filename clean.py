1f085c1052aa11fe6101086011ed483baf67124c
trifonsheykin <trifon.sheykin@gmail.com>
2020-10-31 09:50:21 -0400

    Add files via upload

--- clean.py
+++ clean.py
@@ -28,6 +28,8 @@ from tkinter import messagebox as mb
 import configparser
 import multiprocessing
 import samplerate
+import queue
+import threading
 
 # exe file generation:
 # pyinstaller -F --hidden-import="sklearn.utils._cython_blas" --hidden-import="sklearn.neighbors.typedefs" --hidden-import="sklearn.neighbors.quad_tree" --hidden-import="sklearn.tree._utils" --onefile --noconsole clean.py
@@ -44,10 +46,22 @@ class ArgParser(object):
 def denormalize_wave_minmax(x):
     return ((x - 1.) * (65636./2.)) + 32767.
 
-sd_stream = None
+input_stream = None
+output_stream = None
 file_path = None
+audio_process = False
+sound_thread = None
 device_in = 0
 device_out = 1
+q_in = queue.Queue()
+q_out = queue.Queue()
+ratio_out = 48000/16000
+data_in = np.zeros((8000,1), dtype=np.int16)
+data_temp = np.zeros((8000,1), dtype=np.int16)
+data_out = np.zeros((24000,1), dtype=np.float32)
+i = np.iinfo(data_in.dtype)
+abs_max = 2 ** (i.bits - 1)
+offset = i.min + abs_max
 
 def main():
     seed = 111
@@ -72,8 +86,6 @@ def main():
         global device_out
         device_in = int(config['devices']['device_in'])
         device_out = int(config['devices']['device_out'])
-
-
 
     #Get available devices
     devices = []
@@ -103,19 +115,37 @@ def main():
             config.write(configfile)
 
         segan = None
-        ratio_in = 16000/48000
-        ratio_out = 48000/16000
-        def callback(indata, outdata, frames, time, status):
-            wav = indata[:,0]      
-            wav = samplerate.resample(wav, ratio_in, 'sinc_best')
-            # print(f'{len(wav)} -> {len(wav16)}')
-            wav = normalize_wave_minmax(wav)        
-            wav = pre_emphasize(wav, 0.95)        
-            pwav = torch.FloatTensor(wav).view(1,1,-1)  
-            g_wav, g_c = segan.generate(pwav)        
-            g_wav = denormalize_wave_minmax(g_wav)  
-            g_wav = samplerate.resample(g_wav, ratio_out, 'sinc_best')
-            outdata[:,0] = g_wav
+
+
+        def sound_processing():
+            global audio_process
+            audio_process = True
+            while audio_process:
+                item = q_in.get()
+                print('PROCESSING:')
+                wav = data_in[:,0] 
+                wav = normalize_wave_minmax(wav) 
+                wav = pre_emphasize(wav, 0.95)
+                pwav = torch.FloatTensor(wav).view(1,1,-1) 
+                g_wav, g_c = segan.generate(pwav)
+                g_wav = denormalize_wave_minmax(g_wav)
+                data_temp[:,0] = g_wav 
+                audio = (data_temp.astype('float32') - offset) / abs_max
+                audio = samplerate.resample(audio, ratio_out, 'sinc_best')
+                if q_out.empty():
+                    q_out.put(audio)
+                    data_out[:] = audio
+
+        def input_callback(indata, frames, time, status):
+            print('INPUT:')
+            data_in[:] = indata
+            q_in.put(data_in)
+        
+        def output_callback(outdata, frames, time, status):
+            print('OUTPUT:')
+            if not q_out.empty():
+                d = q_out.get()
+            outdata[:] = data_out
 
         try:
             file_train = 'train.opts' #os.path.join(os.path.dirname(sys.executable), 'train.opts')
@@ -128,12 +158,29 @@ def main():
                 
             segan.G.load_pretrained(file_path, True)
             segan.G.eval()
-            global sd_stream
-            sd_stream = sd.Stream(device=(device_in, device_out),
-                        samplerate=48000, blocksize=12000, #16000 - 1 sec
-                        dtype='int16',
-                        channels=1, callback=callback)
-            sd_stream.start()
+            global input_stream
+            global output_stream
+            global sound_thread
+            # sd_stream = sd.Stream(device=(device_in, device_out),
+            #             samplerate=16000, blocksize=3200, #16000 - 1 sec
+            #             dtype='int16',
+            #             channels=1, callback=callback)
+            input_stream = sd.InputStream(device=device_in, channels=1, dtype='int16',
+                            blocksize=8000, #16000 - 1 sec
+                            samplerate=16000, 
+                            callback=input_callback)
+            
+            output_stream = sd.OutputStream(device=device_out, channels=1, dtype='float32', 
+                            blocksize=24000,
+                            samplerate=48000, 
+                            callback=output_callback)
+            
+            sound_thread = threading.Thread(target=sound_processing)
+
+            sound_thread.start()
+            input_stream.start()
+            output_stream.start()
+
         except Exception as e:
             # parser.exit(type(e).__name__ + ': ' + str(e))
             button_start['state'] = tk.NORMAL
@@ -145,8 +192,16 @@ def main():
         caption_status.set('Conversion stopped')
         button_start['state'] = tk.NORMAL
         button_stop['state']  = tk.DISABLED
-        global sd_stream
-        sd_stream.close()
+        global input_stream
+        global output_stream
+        global audio_process
+        global sound_thread
+        audio_process = False
+        q_in.put(data_in)
+        sound_thread.join()
+        input_stream.close()
+        output_stream.close()
+        print('CONVERSION STOPED')
 
     def open_file(): 
         global file_path
@@ -170,8 +225,6 @@ def main():
     try:
         combobox_in_device.current(int(device_in))
         combobox_out_device.current(int(device_out))
-        # combobox_in_device.set(devices[device_in])
-        # combobox_out_device.set(devices[device_out])
     except Exception as e:
         device_in = 0
         device_out = 1
